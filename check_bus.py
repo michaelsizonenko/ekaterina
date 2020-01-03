4f4b0f4f3e26710c8107bf51a70d2832c1c7395c
Prioritet88 <prioritet@hotel-ekaterina.com>
2020-01-03 17:46:06 +0300

    improve check

--- check_bus.py
+++ check_bus.py
@@ -1,5 +1,12 @@
+import time
 import smbus
 
 bus = smbus.SMBus(1)
 
-print(bin(bus.read_byte(0x38)), bin(bus.read_byte(0x39)))
+state = str(bin(bus.read_byte(0x38))) + '  ' + str(bin(bus.read_byte(0x39)))
+while True:
+	tmp_state = str(bin(bus.read_byte(0x38))) + '  ' + str(bin(bus.read_byte(0x39)))
+	if tmp_state != state:
+		state = tmp_state
+		print(state)
+	time.sleep(0.1)
