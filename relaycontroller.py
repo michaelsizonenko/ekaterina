9dff881b4ce304e666176a0dcb0dadff0de9fb66
michaelsizonenko <dethline@te.net.ua>
2020-01-03 17:31:05 +0200

    add logging to relay controller

--- relaycontroller.py
+++ relaycontroller.py
@@ -13,17 +13,23 @@ class RelayController:
 
     def clear_bit(self, bit):
         value = self.__bus.read_byte(self.__address)
+        print("Clear bit before value {value}".format(value=value))
         value &= ~(1 << bit)
+        print("Clear bit after value {value}".format(value=value))
         self.__bus.write_byte_data(self.__address, 0x09, value)
 
     def set_bit(self, bit):
         value = self.__bus.read_byte(self.__address)
+        print("Set bit before value {value}".format(value=value))
         value |= 1 << bit
+        print("Set bit before value {value}".format(value=value))
         self.__bus.write_byte_data(self.__address, 0x09, value)
 
     def toggle_bit(self, bit):
         value = self.__bus.read_byte(self.__address)
+        print("Toggle bit before value {value}".format(value=value))
         value ^= 1 << bit
+        print("Toggle bit before value {value}".format(value=value))
         self.__bus.write_byte_data(self.__address, 0x09, value)
 
     def check_bit(self, bit):
